6cca0b60c47a2d4875f3c58d613c4fc6a1903416
Paulo Akira <paulo@enabe.com.br>
2024-04-11 09:12:09 -0300

    Model in npz

--- compilers.py
+++ compilers.py
@@ -1,17 +1,10 @@
+# Python libs
 import tvm
 import torch
 import torchvision
+import numpy as np
 from tvm import relay
 
+
 if __name__ == "__main__":
-    # Example model
-    model = torchvision.models.resnet18(pretrained=True)
-    model.eval()
-
-    # Dummy input in the shape the model expects
-    dummy_input = torch.randn(1, 3, 224, 224)
-
-    # Convert the PyTorch model to Relay
-    scripted_model = torch.jit.trace(model, dummy_input).eval()
-    mod, params = relay.frontend.from_pytorch(scripted_model, [("input", dummy_input.shape)])
-    print(f"debug :: mod: {mod}")
+    weights = np.load("assets/model.npz")
